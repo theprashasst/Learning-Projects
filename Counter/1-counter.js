663a98ea8626d062e62d95cad0cd3a3ca7060575
Prashasst Dongre <82633172+theprashasst@users.noreply.github.com>
2024-03-16 03:57:17 +0530

    Add files via upload

---
+++ Counter/1-counter.js
@@ -0,0 +1,45 @@
+let count = 0;
+let isCounting = false;
+let counterInterval;
+
+async function counter() {
+    return new Promise(function(resolve) {
+        setTimeout(function() {
+            count++;
+            resolve(count);
+        }, 1000);
+    });
+}
+
+async function startCounter() {
+    console.log("started");
+    isCounting = true;
+    while (isCounting && count < 100) {
+        let ans= await counter();
+        if (!isCounting) break;
+        document.getElementById('counterDisplay').textContent = ans;
+        console.log("count = ",ans);
+    }
+    if (count===100)console.log('Counter stopped.');
+}
+
+function stopCounter() {
+    
+    isCounting = false;
+    console.log("force stopped");
+}
+document.getElementById('reset').addEventListener('click', function() {
+    count=0;
+    console.log("Reset successfull");
+    stopCounter();
+    document.getElementById('counterDisplay').textContent = 0;
+    
+});
+
+document.getElementById('startBtn').addEventListener('click', function() {
+    startCounter();
+});
+
+document.getElementById('stopBtn').addEventListener('click', function() {
+    stopCounter();
+});
